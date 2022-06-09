a82e017203be53f3e9e48b065c10e80c8f9fa4cb
Nyanthie Mayang Gatluak <nyanthie.mayang@student.moringaschool.com>
2022-06-10 02:45:36 +0300

    commit tree 964b2dca1f81704f59828afe0859886ce0d4510d

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(speed){
   // Write your code here!
+ if(speed <= 400){
+   return 'This one is on me!'
+ }else if(speed > 2000 && speed < 2500){
+   return 'I will gladly take your thirty bucks.'
+ }else {
+   return 'No can do.'
+ }
+  
+    
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+return city ==='NYC' ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if(tip === 'generous') {
+    return "Thank you so much."
+  }
+  else if(tip === 'not as generous'){
+    return "Thank you."
+  }
+  else {
+    return 'Bye.'
+  }
 }
